afc7ae8c8e628c8134eb4f5c2180cf9b18cbfd17
imochoa <18612100+imochoa@users.noreply.github.com>
2026-01-19 19:11:22 +0100

    updated the plugin

--- lua/quickfail/init.lua
+++ lua/quickfail/init.lua
@@ -1,8 +1,200 @@
 local M = {}
 
 ---@require "quickfail.types"
+-- local logic = require("quickfail.logic")
 
-local logic = require("quickfail.logic")
+-- Private vars & fcns
+
+---@enum user_commands
+local CMDS = {
+	reload = "QuickFailReload",
+	quit = "QuickFailQuit",
+	select = "QuickFailSelect",
+	manual = "QuickFailManual",
+}
+---@type Entry
+local DefaultEntry = { cmd = {}, pattern = "*", keycodes = nil }
+local Augroup_name = "quickfail-group"
+
+--- Expand special symbols ~, %, %:p
+---@param cmd string[]
+---@returns string
+local expand_cmd = function(cmd)
+	local exp_str = ""
+	for _, c in ipairs(cmd) do
+		exp_str = exp_str .. vim.fn.expand(c) .. " "
+	end
+	return exp_str
+end
+
+---@type Job[] cache of active jobs
+M.jobs = {}
+
+---@param entry Entry
+---@returns nil
+M.quickfail = function(entry)
+	if #entry.cmd == 0 then
+		vim.notify("Command is required!", vim.log.levels.ERROR, {})
+		return
+	end
+
+	if (entry.pattern or "") == "" then
+    entry.pattern = DefaultEntry.pattern
+	end
+
+	vim.notify(
+		string.format("Will \n\tRunning:%s\n\ton Saving %s\n\tor pressing %s", entry.cmd, entry.pattern, entry.keycodes),
+		vim.log.levels.INFO,
+		{}
+	)
+
+  -- local prev_bufnr = vim.api.nvim_get_current_buf()
+  local prev_window = vim.api.nvim_get_current_win()
+  local prev_cursor = vim.api.nvim_win_get_cursor(prev_window)
+
+	vim.cmd("vsplit new")
+
+	---@type Job
+	local job = {
+		entry = entry,
+		buffer = vim.api.nvim_get_current_buf(),
+		window = vim.api.nvim_get_current_win(),
+		autocmd_id = nil,
+    callback = function() end,
+		-- start a persistent shell in a terminal buffer; this returns a job/channel id
+		chan = vim.fn.jobstart({ vim.o.shell or "sh" }, {
+			term = true,
+			-- cwd={},
+			-- env={},
+		}),
+	}
+
+	-- vim.notify(Job, vim.log.levels.DEBUG, {})
+
+	-- leave Terminal-Job mode (like <C-\><C-n>)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", true)
+
+	-- move cursor to last line (nvim_buf_line_count is 1-indexed) (to tail it....)
+	local last_line = vim.api.nvim_buf_line_count(job.buffer)
+	vim.api.nvim_win_set_cursor(job.window, { last_line, 0 })
+
+  -- recover cursor position in previous window
+  vim.api.nvim_set_current_win(prev_window)
+  vim.api.nvim_win_set_cursor(prev_window, prev_cursor)
+
+
+	job.callback= function()
+		vim.api.nvim_chan_send(job.chan, "clear\n( " .. expand_cmd(entry.cmd) .. " )\n")
+	end
+
+	-- Escape to close the terminal split
+	vim.keymap.set("n", "<Esc>", function()
+		vim.api.nvim_buf_delete(job.buffer, { force = true })
+	end, { buffer = job.buffer })
+
+	if (entry.keycodes or "") ~= "" then
+		vim.keymap.set({ "n" }, entry.keycodes, function()
+			-- vim.notify(cmd, vim.log.levels.DEBUG, {})
+			job.callback()
+		end, {})
+	end
+
+	if (entry.pattern or "") ~= "" then
+		job.autocmd_id = vim.api.nvim_create_autocmd("BufWritePost", {
+			group = vim.api.nvim_create_augroup(Augroup_name, { clear = true }),
+			pattern = entry.pattern,
+			callback = job.callback,
+		})
+	end
+
+	table.insert(M.jobs, job)
+
+	-- run immediately
+	job.callback()
+end
+
+---@returns nil
+function M.manual()
+	-- vim.print(args)
+	local cmd_str = vim.fn.input("cmd to run")
+	cmd_str = cmd_str:gsub("%s+", " ")
+
+	---@type Entry
+	local entry = {
+		cmd = vim.split(cmd_str, "%s+", {}),
+		pattern = vim.fn.input("Pattern (empty for all): "),
+		keycodes = vim.fn.input("Key code e.g. <F13>"),
+	}
+	M.quickfail(entry)
+end
+
+---@returns nil
+function M.select()
+	---@type string[]
+	local display = {}
+	---@type table<string,Entry>
+	local choice2entry = {}
+  local title = ""
+	for i,entry in ipairs(M.config.menu) do
+    title = string.format("[%s] %s", i, entry.title )
+		table.insert(display, title)
+    choice2entry[title] = entry
+	end
+
+	vim.ui.select(display, { prompt = "Choose:" }, function(choice)
+		print("selected:", choice)
+    if not choice then
+      return
+    end
+		M.quickfail(choice2entry[choice])
+	end)
+end
+
+---@returns nil
+function M.quit()
+	for _, job in ipairs(M.jobs) do
+		if job.autocmd_id then
+			vim.api.nvim_del_autocmd(job.autocmd_id)
+		end
+		vim.api.nvim_buf_delete(job.buffer, { force = true })
+	end
+end
+
+--- Initial (Default) configuration
+---@type Config
+M.config = {
+	menu = {
+		{ cmd = { "cat", "%" }, title = "cat", desc = "Test!" },
+		{ cmd = { "python", "%" }, title = "python", desc = "Run python file" },
+		{ cmd = { "just", "%" }, title = "just", desc = "Run Just recipe" },
+	},
+}
+
+--- Setup function that users call
+---@param user_config Config?
+---@return nil
+function M.setup(user_config)
+	M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
+
+	vim.api.nvim_create_user_command(CMDS.quit, M.quit, { desc = "Stop Active QuickFail jobs" })
+	vim.api.nvim_create_user_command(CMDS.reload, function()
+		-- Easy Reloading
+		package.loaded["quickfail"] = nil
+		require("quickfail").setup()
+	end, { desc = "Reload QuickFail plugin" })
+
+	--- use like `vim.cmd[[QuickFailCustom]]`
+	vim.api.nvim_create_user_command(CMDS.manual, M.manual, {
+		desc = "Rerun everything",
+		nargs = "*",
+	})
+
+	vim.api.nvim_create_user_command(CMDS.select, M.select, {
+		desc = "Choose from some options",
+	})
+end
+
+return M
 
 --- --- TODO: defaults, split into something that can be mapped to Fkeys or some other keybinding
 --- ---@param pattern string matching pattern for BufWritePost (defaults to *)
@@ -89,95 +281,3 @@ local logic = require("quickfail.logic")
 ---
 --- 	run_job()
 --- end
-
--- Default configuration
-M.config = {
-	option1 = "default_value",
-	option2 = true,
-}
-
---- Setup function that users call
----@param user_config table Array. Default: { 'world' }.
----@return nil
-function M.setup(user_config)
-	M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
-	-- Initialize your plugin here
-
-	vim.api.nvim_create_user_command("QuickFailReload", function()
-		-- Easy Reloading
-		package.loaded["quickfail"] = nil
-		require("quickfail").setup()
-	end, {})
-
-	--- use like `vim.cmd[[ReRunCmd]]`
-	vim.api.nvim_create_user_command(
-		"ReRunCmd",
-		-- rerecmd,
-		function(args)
-			local keycodes = "<F13>"
-			-- vim.print(args)
-			local pattern = vim.fn.input("Pattern (empty for all): ")
-			local cmd_str = vim.fn.input("cmd to run")
-
-			cmd_str = cmd_str:gsub("%s+", " ")
-			-- -- local trimmed = original:match( "^%s*(.*)" )
-			-- local cmd = vim.split(cmd_str, "%s+", {})
-			--
-			-- cmd_str = cmd_str:match("^%s*(.*)")
-			-- vim.print((" a       b   "):match("%s*(.*)"))
-			-- vim.print((" a       b   "):gsub("%s+", " "))
-			-- -- vim.print(vim.split(cmd_str, "%s+",{}))
-			local cmd = vim.split(cmd_str, "%s+", {})
-
-			-- vim.print(cmd)
-			-- local cmd = { cmd_str }
-
-			--TODO: unpack?
-			logic.rerecmd(pattern, cmd, keycodes)
-		end,
-		{
-			desc = "Rerun everything",
-			nargs = "*",
-		}
-	)
-
-	vim.api.nvim_create_user_command(
-		"ReRunExamples",
-		-- rerecmd,
-		function(args)
-			-- vim.print(args)
-
-			local options = {
-				["python"] = { cmd = { "meow" } },
-				["nix"] = { cmd = { "meow" } },
-				["md"] = { cmd = { "meow" } },
-			}
-
-			local option_keys = {}
-			for k, _ in pairs(options) do
-				table.insert(option_keys, k)
-			end
-
-			vim.ui.select(option_keys, { prompt = "Choose:" }, function(choice)
-				print("selected:", choice)
-				-- vim.print()
-
-				local keycodes = "<F13>"
-				local pattern = "*"
-				local cmd = options[choice].cmd
-
-				--TODO: unpack?
-				logic.rerecmd(pattern, cmd, keycodes)
-			end)
-		end,
-		{
-			desc = "Rerun everything",
-			-- nargs = "*",
-		}
-	)
-	--
-	--
-end
-
-
-return M
--- lua/quickfail/types.lua
+++ lua/quickfail/types.lua
@@ -1,7 +1,79 @@
 ---@meta
 
----@class Job More info with 'help: highlight', 'help: highlight-args'
----@field fcn function what to call
----@field args string|nil
+--- @class (exact) JobStartOpts h: jobstart-options
+--- @field clear_env boolean?
+--- @field cwd string?
+--- @field detach boolean?
+--- @field env table<string, string>?
+--- @field on_exit fun(job_id:integer, exit_code:integer, event:string)|nil
+--- @field on_stdout fun(job_id:integer, data:string[], event:string)|nil
+--- @field on_stderr fun(job_id:integer, data:string[], event:string)|nil
+--- @field overlapped boolean?
+--- @field pty boolean?
+--- @field rpc boolean?
+--- @field stderr_buffered boolean?
+--- @field stdout_buffered boolean?
+--- @field stdin string? "pipe" (default) or "null"
+--- @field term boolean?
+
+
+		  -- clear_env:  (boolean) `env` defines the job environment
+		  --      exactly, instead of merging current environment.
+		  -- cwd:	      (string, default=|current-directory|) Working
+		  --      directory of the job.
+		  -- detach:     (boolean) Detach the job process: it will not be
+		  --      killed when Nvim exits. If the process exits
+		  --      before Nvim, `on_exit` will be invoked.
+		  -- env:	      (dict) Map of environment variable name:value
+		  --      pairs extending (or replace with "clear_env")
+		  --      the current environment. |jobstart-env|
+		  -- |on_exit|:    (function) Callback invoked when the job exits.
+		  -- |on_stdout|:  (function) Callback invoked when the job emits
+		  --      stdout data.
+		  -- |on_stderr|:  (function) Callback invoked when the job emits
+		  --      stderr data.
+		  -- overlapped: (boolean) Sets FILE_FLAG_OVERLAPPED for the
+		  --      stdio passed to the child process. Only on
+		  --      MS-Windows; ignored on other platforms.
+		  -- pty:	      (boolean) Connect the job to a new pseudo
+		  --      terminal, and its streams to the master file
+		  --      descriptor. `on_stdout` receives all output,
+		  --      `on_stderr` is ignored. |terminal-start|
+		  -- rpc:	      (boolean) Use |msgpack-rpc| to communicate with
+		  --      the job over stdio. Then `on_stdout` is ignored,
+		  --      but `on_stderr` can still be used.
+		  -- stderr_buffered: (boolean) Collect data until EOF (stream closed)
+		  --      before invoking `on_stderr`. |channel-buffered|
+		  -- stdout_buffered: (boolean) Collect data until EOF (stream
+		  --      closed) before invoking `on_stdout`. |channel-buffered|
+		  -- stdin:      (string) Either "pipe" (default) to connect the
+		  --      job's stdin to a channel or "null" to disconnect
+		  --      stdin.
+		  -- term:	    (boolean) Spawns {cmd} in a new pseudo-terminal session
+		  --         connected to the current (unmodified) buffer. Implies "pty".
+		  --         Default "height" and "width" are set to the current window
+		  --         dimensions. |jobstart()|. Defaults $TERM to "xterm-256color".
+		  -- width:      (number) Width of the `pty` terminal.
+		  -- height:     (number) Height of the `pty` terminal.
+
+---@class (exact) Entry
+---@field cmd string[] command to run
+---@field keycodes string?
+---@field pattern string?
+---@field title string?
+---@field desc string?
+---@field opts JobStartOpts?
+
+---@class (exact) Job More info with 'help: highlight', 'help: highlight-args'
+---@field entry Entry
+---@field callback function(nil):nil
+---@field buffer integer what buffer it is running in
+---@field window integer what window it is running in
+---@field chan integer channel id
+---@field autocmd_id integer? autocmd id
+
+
+---@class (exact) Config
+---@field menu Entry[]
 
 ---@alias uint8 number (0-255) integers
